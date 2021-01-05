4f1ed652ae23e0cc307bc6500f67d90b2526ae89
Venkata Suresh Dharisi <suresh.dharisi@gmail.com>
2021-01-04 22:46:42 -0500

    Initial version of the project

---
+++ find-city-connect/src/main/java/com/home/code/challenge/findcityconnect/FindCityConnectApplication.java
@@ -0,0 +1,13 @@
+package com.home.code.challenge.findcityconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindCityConnectApplication.class, args);
+	}
+
+}
